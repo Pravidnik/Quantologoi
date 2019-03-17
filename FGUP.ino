16f6bc8038a80f7c0808dca02f0ef8b760a14e98
Alexandr <babeshko.ar@ya.ru>
2019-03-17 15:14:19 +0300

    Add files via upload
    Файлы от Алексея Кондратенко и Александра Бабешко

---
+++ FGUP.ino
@@ -0,0 +1,114 @@
+/*------------------Проект квантологосы. Инсталляция ФГУП. Говорим в рупор - загораются лампочки, сыпятся шарки, 
+ идет сигнал на бульканье в ЛИНКОКО.*/
+ 
+//-----------Настройка диодной ленты
+#include "Adafruit_NeoPixel.h" //Подключение библиотеки для ленты
+#define ledPin 2               //Пин ленты РГБ
+#define ledCount 13             //кол-во диодов в ленте
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(ledCount, 2); // создаем переменную , где указываем кол-во диодов в ленте
+
+//----------Настройка цвета ленты
+#define red 255 //255
+#define green 70 //70
+#define blue 0//0
+
+//----Настройка микрофона
+#define micPin 1  // микрофон подключаем к первому пину аналоговому
+#define limitMicValue 100 //значение микрофона, превысив которое начинаются различные действия
+int micValue;     //Переменная для значений от микрофона
+
+//----Настройка сервопривода
+#include <Servo.h>//Подключаем библиотеку серво
+#define pinServo 4 //Управление сервоприводом 4 пин
+#define timeServo 500 // Время открытия сервы для скатывания шариков
+Servo myservo;//создаем серво объект для контроля сервоприводом
+
+//----Настройка передатчика
+#include <SPI.h>               // Подключаем библиотеку для работы с шиной SPI
+#include <nRF24L01.h>         // Подключаем файл настроек из библиотеки RF24
+#include <RF24.h>              // Подключаем библиотеку РФ24
+RF24 radio(9, 10);             // Создаём объект radio для работы с библиотекой RF24, указывая номера выводов nRF24L01+ (CE, CSN)
+int radioValue;
+int data[1];//создаем массив для передачи данных
+
+
+
+void setup() {
+  Serial.begin (9600);
+  pinMode (micPin, INPUT);  //Настраиваем пин под микрофон на ввод в контроллер
+  strip.begin (); //инициализация ленты
+  strip.setBrightness(255);//яркость светодиодов 
+  myservo.attach(pinServo); // наздначаем управление сервой на 3 пин
+  myservo.write (145);//Начальная позиция сервопривода
+  radio.begin(); //активировать модуль
+  radio.setChannel(5);   // Указываем канал передачи данных (от 0 до 127), 5 - значит передача данных осуществляется на частоте 2,405 ГГц (на одном канале может быть только 1 приёмник и до 6 передатчиков)
+  radio.setDataRate     (RF24_1MBPS); // Указываем скорость передачи данных (RF24_250KBPS, RF24_1MBPS, RF24_2MBPS), RF24_1MBPS - 1Мбит/сек
+  radio.setPALevel      (RF24_PA_HIGH); // Указываем мощность передатчика (RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm, RF24_PA_HIGH=-6dBm, RF24_PA_MAX=0dBm)
+  radio.openWritingPipe (0x1234567890LL);// Открываем трубу с идентификатором 0x1234567890 для передачи данных (на одном канале может быть открыто до 6 разных труб, которые должны отличаться только последним байтом идентификатора)
+}
+
+
+void LED () {       //Создание функции для светодиодной ленты
+  for (int i = 0; i <= ledCount ; i++) {
+    strip.setPixelColor (i, red, green, blue);
+  } 
+  strip.show ();
+  delay(500);
+  for (int i = 0; i < ledCount; i++) 
+  { 
+    strip.setPixelColor(i, strip.Color(0, 0, 0));
+  } 
+  strip.show ();
+  delay(2000);
+    for (int i = 0; i <= ledCount ; i++) {
+    strip.setPixelColor (i, red, green, blue);
+  } 
+  strip.show ();
+  delay(1000);
+  
+  for (int i = 0; i < ledCount; i++) 
+  { 
+    strip.setPixelColor(i, strip.Color(0, 0, 0));
+  } 
+  strip.show ();
+  delay(1000);
+
+
+for (int i = 0; i <= ledCount ; i++) {
+    strip.setPixelColor (i, red, green, blue);
+  } 
+  strip.show ();
+  
+} 
+
+void servo () {    //Создание функции для серворивода
+  myservo.write (70);
+  delay (timeServo);
+  myservo.write (145);
+}
+
+void loop() {
+  radioValue = 0;
+  data[0] = radioValue;
+  micValue =  analogRead(micPin);
+  Serial.println (micValue);
+  radio.write(&data, sizeof(data)); // отправляем данные из массива data указывая сколько байт массива мы хотим отправить. Отправить данные можно с проверкой их доставки: if( radio.write(&data, sizeof(data)) ){данные приняты приёмником;}else{данные не приняты приёмником;}
+
+  if (micValue > limitMicValue) { //если значение с микрофона превысит лимитированное значение, то...
+   radioValue = 1;
+   data [0] = radioValue;
+   radio.write(&data, sizeof(data)); // отправляем данные из массива data указывая сколько байт массива мы хотим отправить. Отправить данные можно с проверкой их доставки: if( radio.write(&data, sizeof(data)) ){данные приняты приёмником;}else{данные не приняты приёмником;}
+   LED (); 
+    delay (10000); //время задержки пока булькает ЛИНКОКО
+    servo();
+   for (int i = 0; i <= ledCount ; i++) {
+    strip.setPixelColor (i, 0, 0, 0);
+  } 
+  strip.show (); 
+  }
+  Serial.println (micValue);
+ 
+}
+
+
+
---
+++ Microscope_SERVO.ino
@@ -0,0 +1,103 @@
+#define PIN_NIZ     9 // светодиод снизу
+#define PIN_LEV     6 // светодиод слева
+#define PIN_VERH    7 // светодиод сверху
+#define PIN_LBOT    12 // лента в левом ботинке
+#define PIN_RBOT    11 // лента в правом ботинке
+#define PIN_IR      A3 // пин для подключения датчика расстояния. Обязательно аналоговый!
+#define PIN_SERVO   3 // пин для подключения серво-привода
+#define NUM_LEDS    20 // кол-во светодиодов в ленте
+#define DIST_NEAR   30 // ближняя дистанция срабатывания
+#define DIST_MID    40 // средняя
+#define DIST_FAR    60 // дальняя
+#define SERVO_START 170  // начальное положение серво-привода
+#define SERVO_END   30 // конечное положение серво-привода
+#define ARRAY       5 // массив для вычисления среднего числа при измерении дистанции
+
+#include <SharpIR.h>
+#include <Servo.h>
+
+SharpIR sensor(PIN_IR, 1080);
+Servo myServo;
+
+int posNow;
+int distArray[ARRAY];
+int distAverage;
+
+void setup() {
+  myServo.attach(PIN_SERVO);
+  myServo.write(SERVO_START);
+  Serial.begin(9600);
+
+  // записываем начальные значения расстояни в массив
+  for (int i = 0; i < ARRAY; i++) {
+    distArray[i] = 30;
+  }
+}
+
+void loop() {
+  for (int i = 0; i < ARRAY; i++) {
+    if (i == ARRAY - 1) { // если добрались до последнего элемента
+      distArray[ARRAY - 1] = sensor.distance(); // получаем расстояние с датчика и сохраняем в последний элемент массива
+    } else {
+      distArray[i] = distArray[i + 1]; // двигаем все значения в массие назад на один
+    }
+  }
+
+  arrayAverage(); // корректирует distAverageage в зависимости от значений в массиве distArray
+  arrayDisplay(); // выведение значений массива и их среднего значения на экран - ВАЖНО закомментить его при "рабочей прошивке", так как это влияет на серву - глючить может
+
+  // поднимаем или опускаем серву от полученных значений
+  posNow = myServo.read();
+  if (distAverage < DIST_NEAR) {
+    /*servoUp();*/
+    myServo.write(SERVO_END);
+    
+  } else {
+    myServo.write(SERVO_START);
+    /*servoDown();*/
+  }
+
+  delay(200); // выставляем задержку, чтобы показания с датчика собирались не слишком часто
+}
+
+void arrayAverage() {
+  int distSumForAverage = 0;
+  for (int i = 0; i < ARRAY; i++)
+  {
+    distSumForAverage = distArray[i] + distSumForAverage;
+  }
+  distAverage = distSumForAverage / ARRAY; // вычисление средней длины из последний пяти, например, значений
+  
+}
+
+void arrayDisplay() {
+  for (int i = 0; i < ARRAY; i++) {
+    Serial.print(distArray[i]);
+    Serial.print(" ");
+  }
+  Serial.print("/ average = ");
+  Serial.print(distAverage);
+  Serial.println("");
+}
+
+void servoUp() {
+  if (posNow > SERVO_END) {
+    for (int i = posNow; i <= SERVO_END; i--) {
+      myServo.write(i);
+      delay (7);
+    }
+  } else {
+    myServo.write(SERVO_END);
+  }
+}
+
+void servoDown() {
+  if (posNow < SERVO_START) {
+    for (int i = posNow; i <= SERVO_START; i++) {
+      myServo.write(i);
+      delay (7);
+    }
+  } else {
+    myServo.write(SERVO_START);
+  }
+}
