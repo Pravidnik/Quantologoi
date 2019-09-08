a9ac18a526f80569334618bdffb1698af9847c43
Alexandr <babeshko.ar@ya.ru>
2019-09-08 18:08:32 +0300

    Переведено на мотор
    Серво-привод заменен на мотор;
    Единичная светодиод заменен ЛЕД-лентой;
    Убраны дилейи и все сделано на миллис

--- komon.ino
+++ komon.ino
@@ -1,180 +1,150 @@
-//----------Настройка сонара--------
-#include <NewPing.h>
-#define TRIG 3
-#define ECHO 7
-#define DIST_MAX 300
-#define REACT_DIST 15
+const int trigPin = 9;
+const int echoPin = 10;
+// defines variables
+long duration;
+int distance;
+int ENA = 3; // скорость мотора
+int IN1 = 4; // контакт 1 мотора
+int IN2 = 5; // контакт 2 мотора
+int ENB = 6;  // яркость светодиода
+int IN3 = 7; // плюс светодиодов
+int IN4 = 8; // минус светодиодов
 
-NewPing sonar(TRIG, ECHO, DIST_MAX);
+unsigned long timerRotate;
+unsigned long timerLed;
 
-int Dst;
-//-------Настройки сервопривода-------------------
-#define SERVO_PIN 9
-#define MIN_POS 10// Минимальная позиция сервопривода
-#define MAX_POS 170 // Максимальная позиция сервопривода
-#define ON_SPEED_SERVO 100
-#define OFF_SPEED_SERVO 100
+int flagRotate = 0;
+int flagLed = 0;
+int flagDist = 0;
+const long interval = 4000;           // interval at which to blink (milliseconds)
 
-
-#define ON_TIME 5000
-
-#define MIN_DELAY 150 // +25
-#define MAX_DELAY 50 
-
-// пример с "бегущей радугой" для библиотеки FastLED
-#define NUM_LEDS 4
-#include "FastLED.h"
-#define PIN 13
-#define BRIGHTNESS 255
-#define RAINBOW_SPEED 15  // скорость движения радуг
-#define STEP_RAINBOW 400// Чем больше ставишь, тем меньше шаг радуги
-CRGB leds[NUM_LEDS];
-byte counter;
-unsigned long lastled;
-unsigned long LastOn;
-int ledstatus;
-#include <Servo.h>
-Servo servo;
-
-#define TIME_SERVO_OFF 1054
-
-unsigned long LastServoOff;
-
-int Pos;
-unsigned long TimeServo;
-int Dist;
-void setup() {
-  FastLED.addLeds<WS2811, PIN, GRB>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
-  FastLED.setBrightness(255);
-  pinMode(PIN, OUTPUT);
-  servo.attach(SERVO_PIN);
-  Serial.begin(9600);
-}
-
-void loop228()
+void setup()
 {
-  Sonar(150);
-  //delay(527);
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  //pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  Serial.begin(9600); // Starts the serial communication
 }
 
 void loop()
 {
-  Sonar(150);
-  if (Dst > 5 && Dst < 30)
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+
+  motorActive();
+  ledActive();
+
+  if (distance <= 20 && distance > 10)
   {
-   // delay(MIN_DELAY);
-    delay(350);
-    for (int i = 0; i <= MAX_DELAY; i++)
+    flagDist = 1;
+  }
+}
+
+// подсветка
+void ledActive()
+{
+  if (flagDist == 1)
+  {
+    if (millis() - timerLed >= (interval / 8) && flagLed == 0) {
+      timerLed = millis();
+      analogWrite(ENA, 255);
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      flagLed = 1;
+      Serial.println("ON");
+    }
+    if (millis() - timerLed >= (interval / 8) && flagLed == 1) {
+      timerLed = millis();
+      analogWrite(ENA, 255);
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      flagLed = 0;
+      Serial.println("OFF");
+    }
+  }
+  else {
+    // выключаем все
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+  }
+}
+
+// вращение моторчика
+void motorActive()
+{
+  if (flagDist == 1) {
+    if (millis() - timerRotate >= (interval / 4) && flagRotate == 0)
     {
-      Sonar(125);
-      if(Dst > 5 && Dst < 30){
-      LastOn = millis();
-      while (millis() - LastOn < ON_TIME)
-      {
-        onAll();
-      }
-      break;
+      motorLeft();
+      timerRotate = millis();
+      flagRotate = 1;
     }
-    delay(1);
-    Serial.print("LOX "); Serial.println(Dst);
+    if (millis() - timerRotate >= interval && flagRotate == 1)
+    {
+      motorStop();
+      timerRotate = millis();
+      flagRotate = 2;
     }
-    lightOn();
-    ServoOff();
-    delay(257);
+    if (millis() - timerRotate >= (interval / 4) && flagRotate == 2)
+    {
+      motorRight();
+      timerRotate = millis();
+      flagRotate = 3;
+    }
+    if (millis() - timerRotate >= interval && flagRotate == 3)
+    {
+      motorStop();
+      timerRotate = millis();
+      flagRotate = 0;
+      flagDist = 0;
+    }
   }
-  if (millis - LastServoOff > TIME_SERVO_OFF)
-  {
-    lightOn();
-    ServoOff();
-    delay(257);
-    LastServoOff = millis();
-  }
-
-}
-
-void onAll() {
-  while (Pos < MAX_POS)
-  {
-    ServoRight();
-    light(400);
-
-  }
-
-  while (Pos > MIN_POS)
-  {
-    ServoLeft();
-    light(400);
+  else {
+    // выключаем все
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
   }
 }
 
-void ServoOff()
+void motorLeft()
 {
- // Pos = servo.read();
-  servo.write(90);
- // Serial.println(Pos);
-
+  analogWrite(ENB, 100);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  Serial.println("<===");
 }
 
-void ServoRight() //Плавный подьем сервопривода
+void motorRight()
 {
-  if (millis() - TimeServo > ON_SPEED_SERVO && Pos < MAX_POS)
-  {
-    Pos++;
-    servo.write(Pos);
-    TimeServo = millis();
-    // Serial.println(Pos);
-  }
+  Serial.println("===>");
+  analogWrite(ENB, 100);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
 }
 
-void ServoLeft() //Плавный подьем сервопривода
+void motorStop()
 {
-  if (millis() - TimeServo > OFF_SPEED_SERVO && Pos > MIN_POS)
-  {
-    Pos--;
-    servo.write(Pos);
-    TimeServo = millis();
-    // Serial.println("ON");
-  }
-}
-
-void lightOn()
-{
-for (int i = 0; i < NUM_LEDS; i++ ) {
-      leds[i] = CHSV(0 , 0, 0);  // HSV. Увеличивать HUE (цвет)
-      FastLED.show();
-      // умножение i уменьшает шаг радуги
-    }
-}
-
-void light(int sped) {
-  if (millis() - lastled > sped && ledstatus == 0)
-  {
-    for (int i = 0; i < NUM_LEDS; i++ ) {
-      leds[i] = CHSV(0 , 0, 255);  // HSV. Увеличивать HUE (цвет)
-      FastLED.show();
-      // умножение i уменьшает шаг радуги
-    }
-    lastled = millis();
-    ledstatus = 1;
-  }
-
-
-  if (millis() - lastled > sped && ledstatus == 1)
-  {
-    for (int i = 0; i < NUM_LEDS; i++ ) {
-      leds[i] = CHSV(0 , 0, 0);  // HSV. Увеличивать HUE (цвет)
-      FastLED.show();
-      // умножение i уменьшает шаг радуги
-    }
-    lastled = millis();
-    ledstatus = 0;
-  }
-
-
-}
-
-void Sonar(int timeStop) {
-  Dst = sonar.ping_cm();
-  Serial.println(Dst);
-  delay(timeStop);
+  Serial.println("=");
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
 }
